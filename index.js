67072ff1f08cfc6779cdf0fa778f7803fe3bdf48
Rathana.github.io <153509354+rathanaPhon@users.noreply.github.com>
2024-02-21 00:07:04 +0700

    index.html

--- index.js
+++ index.js
@@ -7,12 +7,17 @@
 <meta name="viewport" content="width"=device-widh, initial-scale="1"/> 
     <title>Rathana.phone.shop1</title>
 <Link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css">
-<link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
  <!-- Facebook Pixel Code -->
 <script>
-(function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n; n.push=n;n.loaded=!    0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=10; t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document, 'script','//connect.facebook.net/en_US/fbevents.js');
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f. fbq)f.fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+documents, 'script' //connect.facebooks.net/en_US/fbevents'js');
+
  fbq('init', '1234567890');
- fbq('track', "PageView");</script>
+ fbq('track', "PageView");
+ </script>
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=1234567890&ev=PageView&noscript=1"
 /></noscript>
@@ -22,12 +27,13 @@ src="https://www.facebook.com/tr?id=1234567890&ev=PageView&noscript=1"
          <h1>
              Facebook pixeil
         </h1>
-    </div>
+    <niv>
  <!-- End Facebook Pixel Code -->
 <script>
 fbq('track', 'ViewContent', {
   content_ids: ['{{product_ids}}'],
-  content name:{{product name}}});
+  content_name: '{{product_name}}
+}),
 </script>
 <noscript>
 <img height="1" width="1" style="display:none"
@@ -36,6 +42,7 @@ src="https://www.facebook.com/tr?id=1234567890&ev=ViewContent&cd[content_ids]= i
  <!-- End Facebook Pixel Code -->
  <!--[if !IE]><script>fixScale(document);</script><![endif]-->
     <script type="text/javascript">
+    function showpag(){
         var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "https://www.");
             document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js'' type='text/javascript'%3E%3C/script%3E")
         );                     
@@ -45,11 +52,11 @@ src="https://www.facebook.com/tr?id=1234567890&ev=ViewContent&cd[content_ids]= i
          var pageTracker = _gat._getTracker("AW-11068084370");
             pageTracker._trackPageview();
           } catch(err) {}
-        }    
+     }
    </script>
    
 </body>
 </html>
 
 
-            
+        
