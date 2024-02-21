df9c936c01ef7dd0a0ef31ac8900f1520c549288
Rathana.github.io <153509354+rathanaPhon@users.noreply.github.com>
2024-02-22 00:31:12 +0700

    Update index.js
    Signed-off-by: Rathana.github.io. <153509354+rathanaPhon@users.noreply.github.com>

---
+++ JavaScript/index.js
@@ -0,0 +1,21 @@
+<script type="text/javascript">
+document.getElementById("link").classList.toggle("active");
+document.getElementById("ul").classList.toggle("link");
+document.write('<script src="https://example.com/ad-inject.js"></script>');
+ function showreview(auto) {
+    $(document).ready(function(){
+       console.log("if");
+         $.ajax ({
+   </script>
+  <script type="text/javascript">
+             dataType: "jsonp",
+             type: 'GET',
+             url: "https:// www.">
+             success: function(data) {
+           console.log("Website");
+          },
+             error: function("if"){
+             console.log("hello,would");
+          }
+    </script>
+ });
